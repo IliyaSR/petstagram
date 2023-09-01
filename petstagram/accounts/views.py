a7f9994f01350b27b7bee6c4c37c3dadc3d18984
Iliya Raev <131691288+IliyaSR@users.noreply.github.com>
2023-09-01 21:26:56 +0300

    Add files via upload

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,10 +1,13 @@
 from django.urls import reverse_lazy
+from django.templatetags.static import static
 from django.views import generic as views
-from django.shortcuts import render
-from django.contrib.auth import views as auth_views
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views, get_user_model
 
 from petstagram.accounts.forms import PetstagramUserCreateForm, LoginForm, PetstagramUserEditForm
 from petstagram.accounts.models import PetstagramUser
+
+UserModel = get_user_model()
 
 
 class UserRegisterView(views.CreateView):
@@ -24,8 +27,28 @@ class UserLogoutView(auth_views.LogoutView):
     next_page = reverse_lazy('login')
 
 
-def profile_details_page(request, pk):
-    return render(request, template_name='accounts/profile-details-page.html')
+class UserDetailsView(views.DetailView):
+    model = UserModel
+    template_name = 'accounts/profile-details-page.html'
+
+    profile_image = static('images/person.png')
+
+    def get_profile_image(self):
+        if self.object.profile_picture is not None:
+            return self.object.profile_picture
+        return self.profile_image
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        totals_like_count = sum(p.like_set_count() for p in self.object.photo_set.all())
+
+        context.update({
+            'totals_like_count': totals_like_count,
+        })
+
+        context['profile_image'] = self.get_profile_image()
+
+        return context
 
 
 class UserEditView(views.UpdateView):
@@ -37,5 +60,10 @@ class UserEditView(views.UpdateView):
         return reverse_lazy('profile_details', kwargs={'pk': self.object.pk})
 
 
-def profile_delete_page(request, pk):
-    return render(request, template_name='accounts/profile-delete-page.html')
+class UserDeleteView(views.DetailView):
+    model = PetstagramUser
+    template_name = 'accounts/profile-delete-page.html'
+
+    def post(self, *args, pk):
+        self.request.user.delete()
+        return redirect('home')
